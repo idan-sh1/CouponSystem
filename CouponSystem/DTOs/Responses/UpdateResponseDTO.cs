c9eb0857f261275bb8b3d1b499e1fd99037c772c
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 23:18:53 +0200

    Add update response DTO for disable/enable user method

---
+++ CouponSystem/DTOs/Responses/UpdateResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿using CouponSystem.Models;
+
+namespace CouponSystem.DTOs.Responses
+{
+    public class UpdateResponseDTO
+    {
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+        public User? User { get; set; }
+    }
+}
