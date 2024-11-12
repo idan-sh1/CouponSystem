004c861341a61a19288527603263b7f1e79ebc6d
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 21:53:21 +0200

    Add Users Controller

---
+++ CouponSystem/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using CouponSystem.Data;
+using CouponSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly JwtHandler _jwtHandler;
+
+        public UsersController(UserManager<User> userManager, JwtHandler jwtHandler)
+        {
+            _userManager = userManager;
+            _jwtHandler = jwtHandler;
+        }
+    }
+}
