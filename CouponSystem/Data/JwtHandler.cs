b091f9e38534642d20f42c8c47dd747d7d0d82f2
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 21:41:51 +0200

    Add “JwtHandler” Class

---
+++ CouponSystem/Data/JwtHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace CouponSystem.Data
+{
+    public class JwtHandler
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConfigurationSection _jwtSettings;
+
+        public JwtHandler(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _jwtSettings = _configuration.GetSection("JWTSettings");
+        }
+    }
+}
