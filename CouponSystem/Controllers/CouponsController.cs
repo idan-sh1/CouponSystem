f0fa59bc919ddde3b31773eeb4294c0f1c35a22c
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 23:51:49 +0200

    Add Coupons Controller

---
+++ CouponSystem/Controllers/CouponsController.cs
@@ -0,0 +1,21 @@
+﻿using CouponSystem.Data;
+using CouponSystem.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponsController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<User> _userManager;
+
+        public CouponsController(AppDbContext dbContext, UserManager<User> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+    }
+}
