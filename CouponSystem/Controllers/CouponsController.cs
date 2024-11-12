bb6b42acdaf76b89460f98981d0a94f7f099afd1
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 23:59:13 +0200

    Add EditCoupon method

--- CouponSystem/Controllers/CouponsController.cs
+++ CouponSystem/Controllers/CouponsController.cs
@@ -85,6 +85,73 @@ namespace CouponSystem.Controllers
         }
 
         // ----------------------------------------------------------------- //
+                                   // Edit Coupon
+                             // (requires "Admin" role)
+        // ----------------------------------------------------------------- //
+
+        [HttpPut("id:{id}")]
+        [Authorize(Roles = "Admin")]
+        public IActionResult EditCoupon([FromBody] EditCouponDTO couponDTO, int id)
+        {
+            // If couponDTO is empty --> Return 400 Bad Request
+            if (couponDTO == null)
+            {
+                return BadRequest(new { Error = "Invalid coupon data." });
+            }
+
+            // Find the coupon by Id
+            var coupon = _dbContext.Coupons.Where(c => c.Id == id).FirstOrDefault();
+
+            // If coupon doesn't exist --> Return 404 Not Found
+            if (coupon == null)
+            {
+                return NotFound(new { Error = "The coupon does not exist." });
+            }
+
+            // Add errors list
+            var errors = new List<string>();
+
+            // Create regext to check the coupon code contains both letters and numbers and at leeast 6 characters
+            Regex r = new Regex(@"^(?=.*?\d)(?=.*?[a-zA-Z])[a-zA-Z\d]{6,}$");
+
+            if (!r.IsMatch(couponDTO.Code))
+            {
+                // Add error
+                errors.Add("Coupon code must have at least 6 characters, contain both letters and numbers.");
+            }
+
+            // Check if coupon have neither amount or precent off values
+            if ((!couponDTO.AmountOff.HasValue || couponDTO.AmountOff.Value == 0) &&
+                (!couponDTO.PercentOff.HasValue || couponDTO.PercentOff.Value == 0))
+            {
+                // Add error
+                errors.Add("Coupon code must have either amount or percent value greater than 0.");
+            }
+
+            // If there are errors --> Return 400 Bad Request
+            if (errors.Count > 0)
+            {
+                return BadRequest(new { Errors = errors });
+            }
+
+            // Copy data from DTO to Coupon
+            coupon.Code = couponDTO.Code;
+            coupon.Description = couponDTO.Description;
+            coupon.AmountOff = couponDTO.AmountOff;
+            coupon.PercentOff = couponDTO.PercentOff;
+            coupon.CombinedDiscounts = couponDTO.CombinedDiscounts;
+            coupon.ExpiresAt = couponDTO.ExpiresAt;
+            coupon.MaxUses = couponDTO.MaxUses;
+
+            // Update coupon and save changes to db
+            _dbContext.Coupons.Update(coupon);
+            _dbContext.SaveChanges();
+
+            // 200 OK with the coupon in the response body
+            return Ok(coupon);
+        }
+
+        // ----------------------------------------------------------------- //
                               // Display Coupon By Id
                              // (requires "Admin" role)
         // ----------------------------------------------------------------- //
