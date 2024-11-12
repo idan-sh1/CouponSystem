b229b1c338436e05af15cf271667ea43d10a0e84
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-13 00:50:44 +0200

    Add Reports Controller

---
+++ CouponSystem/Controllers/ReportsController.cs
@@ -0,0 +1,23 @@
+﻿using CouponSystem.Data;
+using CouponSystem.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CouponSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")] // requires "Admin" role
+    public class ReportsController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+        private readonly UserManager<User> _userManager;
+
+        public ReportsController(AppDbContext dbContext, UserManager<User> userManager)
+        {
+            _dbContext = dbContext;
+            _userManager = userManager;
+        }
+    }
+}
