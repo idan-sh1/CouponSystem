cd3c90419050e670fb850c0ab48f89ddbdc361d6
idan_ <idan_@DESKTOP-44R4SK8>
2024-11-12 21:17:20 +0200

    Add "AppDbContext" class (inherit from identity)

---
+++ CouponSystem/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CouponSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CouponSystem.Data
+{
+    public class AppDbContext : IdentityDbContext<User, Role, string>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
